caf4f1f338b01aa4d7398888012f6f842fbb1195
KeyzDev <86132907+Marigiko@users.noreply.github.com>
2022-05-15 20:52:29 -0300

    Update

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const db = require('../../models');
 const passport = require('../../config/passport');
 const authenticator = require('authenticator');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
   console.log(req);
@@ -19,6 +21,7 @@ router.post('/singup', (req, res) => {
     lastName: req.body.lastName,
     email: req.body.email,
     password: req.body.password,
+    isValid: false,
   })
     .then((dbResponse) => {
       res.json(dbResponse);
@@ -54,6 +57,58 @@ router.get('/user_data', (req, res) => {
       id: req.user.id,
     });
   }
+});
+
+const sendEmail = (email, link, name) => {
+  let transport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    }
+  });
+
+  var mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Email confirmation",
+    html: `${name}<br>To activate your account <a href="${link}">Click here</a>`
+  }
+
+  transport.sendMail(mailOptions, (error, response) => {
+    if(error) {
+      console.log(error);
+    } else {
+      console.log("Message sent");
+    }
+  });
+}
+
+var Rand,Host,link;
+
+router.get('/send/:to/:name', (req, res) => {
+  const { name } = req.params;
+  const { to } = req.params;
+
+  Rand = Math.floor((Math.random() * 100) + 54);
+  console.log("HERE IS THE RAND",Rand);
+  Host = req.get('Host');
+  link = `http://${req.get('Host')}/verify/${Rand}`;
+
+  sendEmail(to, link, name);
+  res.json("Email sent");
+})
+
+router.get('/verify/:id',(req, res) => {
+  const { id } = req.params;
+
+  if(id == Rand) {
+    console.log("email if verified"); 
+    res.end(`<h1>Email is been Successfully verified`);
+  } else {
+    console.log("email is not verified");
+    res.end("<h1>Bad Request</h1>");
+    }
 });
 
 module.exports = router;
